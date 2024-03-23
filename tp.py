2d89f25a0df1bec982268fd018f78da40ad68317
TranquilTanisha <tanisha.kaur@somaiya.edu>
2024-03-23 22:40:33 +0530

    minor changes

--- tp.py
+++ tp.py
@@ -1,9 +1,24 @@
-import pyttsx3
+import requests
 
-speaker=pyttsx3.init()
-voices=speaker.getProperty('voices')
-speaker.setProperty('voice', voices[1].id)
-speaker.setProperty('rate', 120)
+def find_location_on_google_maps(latitude, longitude):
+    google_maps_url = f"https://www.google.com/maps/place/{latitude},{longitude}"
+    return google_maps_url
 
-speaker.say('Hello')
-speaker.runAndWait()
+def get_current_location():
+    try:
+        response = requests.get('https://ipapi.co/json/')
+        data = response.json()
+        latitude = data.get('latitude')
+        longitude = data.get('longitude')
+        return latitude, longitude
+    except Exception as e:
+        print("Error fetching location:", e)
+        return None, None, None, None
+
+latitude, longitude = get_current_location()
+url= find_location_on_google_maps(latitude, longitude)
+if latitude is not None and longitude is not None:
+    print(f"Current location: Latitude - {latitude}, Longitude - {longitude}")
+    print(url)
+else:
+    print("Failed to fetch current location.")
